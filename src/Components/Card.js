a6e05c1f72d1d59ca56b1586eebf4580eaf69ee7
Isanchez <isanchez@tiempodevelopment.com>
2018-07-15 19:07:21 -0500

    + add search and css changes

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 //destructure props
 //const { id, name, email } = props;
-
-
 const Card = ({id, name, email}) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
---
+++ src/Components/Searchbox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Searchbox = ({searchfield, searchChange}) => {
+    return (
+        <div className='tc pa2'>   
+            <input 
+            className='pa3 ba b--green bg-lightest-blue'
+            type='search' 
+            placeholder='Search robots'
+            onChange ={searchChange} />
+        </div>
+        
+    );
+}
+
+export default Searchbox;
